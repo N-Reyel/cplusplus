9b4495ddff05579d0942877bfa792b4cb7162aa1
reyel <nicholasreyel@gmail.com>
2024-03-22 00:50:35 -0300

    Sequência de fibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int fibonacci(int n);
+
+int main(){
+    int n;
+    std::cin >> n;
+    std::cout << fibonacci(n) << std::endl;
+
+    return 0;
+}
+
+int fibonacci(int n){
+    if(n == 0){
+        return 0;
+    }else if(n == 1){
+        return 1;
+    }else{
+        return(fibonacci(n-1) + fibonacci(n-2));
+    }
+}
