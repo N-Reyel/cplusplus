b756afda2eb7fcd2de5922e2a61857a6d31f6db2
reyel <nicholasreyel@gmail.com>
2024-03-24 00:52:44 -0300

    torre de hanoi

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -3,10 +3,22 @@
 int hanoi(int n);
 
 int main(){
-
+    int n;
+    std::cin >> n;
+    std::cout << hanoi(n);
+    
     return 0;
 }
 
 int hanoi(int n){
-    
+    switch (n){
+        case 1: {
+            return 1;
+            break;
+        }
+        default:{
+            return (2*hanoi(n-1) + 1);
+            break;
+        }
+    }
 }
