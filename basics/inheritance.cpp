d7a5882904c85797041ae8573043722a4aedc498
N-Reyel <nicholasreyel@gmail.com>
2024-05-06 05:09:38 -0300

    inheritance basics

--- basics/inheritance.cpp
+++ basics/inheritance.cpp
@@ -3,29 +3,37 @@
 class Vampire{
     public:
         void drinkBlood(){
-            std::cout << "Drinks blood" << std::endl;
+            std::cout << "Drinks blood ";
         }
         void noSun(){
-            std::cout << "Dies in the Sun" << std::endl;
+            std::cout << "Dies in the Sun ";
         }
         void immortal(){
-            std::cout << "Is Immortal" << std::endl;
+            std::cout << "Is Immortal ";
         }
 }; 
 
 class Dracula : public Vampire{
     public:
         void vampires(){
-            std::cout << "Creates other vampires" << std::endl;
+            std::cout << "Creates other vampires ";
         }
 };
 
 
 int main(){
     Vampire lestat;
+    std::cout << "Lestat "; 
+    lestat.immortal();
     lestat.drinkBlood();
+    lestat.noSun();
+    std::cout << "\n";
+
+
 
     Dracula vlad;
+    std::cout << "Vlad ";
+    vlad.immortal();
     vlad.drinkBlood();
     vlad.vampires();
 
